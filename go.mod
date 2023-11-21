fd94c1626fa4b3c8c164aa146bb681fb55d26ce0
Narongrit089 <641463014@crru.ac.th>
2023-11-21 14:19:12 +0700

    edit main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Narongrit089/go-network2.git
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	//Hanble request
+	r := mux.NewRouter()
+
+	r.HandleFunc("/book/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		//create cal
+		b := []byte("<h1>Book: " + title + "</h1><br><h2>Pade: " + page + "</h2>")
+
+		// fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+		w.Write(b)
+	})
+
 	// "/" is the routh path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!!") // Write data to response
@@ -17,9 +34,9 @@ func main() {
 	})
 
 	// static/ is the routh path
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// fs := http.FileServer(http.Dir("static/"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	//listen
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
