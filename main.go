99be30364a17b4434b6efb0c4798baf1458b1d6c
Narongrit089 <641463014@crru.ac.th>
2023-11-21 13:35:06 +0700

    edit main.go

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ func main() {
 		fmt.Fprintf(w, "Hello World!!") // Write data to response
 	})
 
+	// "/about" is the routh path
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "HaHa") // Write data to response
+	})
+
+	// static/ is the routh path
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	//listen
 	http.ListenAndServe(":8080", nil)
 }
