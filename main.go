b5de318381a6a59aa6097d7cecea94b51233566f
Narongrit089 <641463014@crru.ac.th>
2023-11-21 15:55:30 +0700

    edit main.go

--- main.go
+++ main.go
@@ -3,40 +3,98 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	//Hanble request
+	// Handle request
 	r := mux.NewRouter()
 
-	r.HandleFunc("/book/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/cal/{num1}/{operation}/{num2}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
+		num1Str := vars["num1"]
+		operation := vars["operation"]
+		num2Str := vars["num2"]
 
-		//create cal
-		b := []byte("<h1>Book: " + title + "</h1><br><h2>Pade: " + page + "</h2>")
+		// Convert num1 and num2 to integers
+		num1, err := strconv.Atoi(num1Str)
+		if err != nil {
+			http.Error(w, "Invalid number: "+num1Str, http.StatusBadRequest)
+			return
+		}
 
-		// fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-		w.Write(b)
+		num2, err := strconv.Atoi(num2Str)
+		if err != nil {
+			http.Error(w, "Invalid number: "+num2Str, http.StatusBadRequest)
+			return
+		}
+
+		// Perform the requested operation
+		result := calculate(num1, operation, num2)
+
+		// Create response string
+		response := fmt.Sprintf(`
+    <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    text-align: center;
+                    padding: 50px;
+                }
+                h1 {
+                    color: #333;
+                }
+                p {
+                    font-size: 18px;
+                    color: #666;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Result</h1>
+            <p>The result of the operation is: <strong>%d</strong></p>
+        </body>
+    </html>
+`, result)
+
+		// Write the response to the client
+		w.Write([]byte(response))
 	})
 
-	// "/" is the routh path
+	// "/" is the route path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!!") // Write data to response
 	})
 
-	// "/about" is the routh path
+	// "/about" is the route path
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "HaHa") // Write data to response
 	})
 
-	// static/ is the routh path
-	// fs := http.FileServer(http.Dir("static/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	//listen
+	// Listen and serve
 	http.ListenAndServe(":8080", r)
+}
+
+// Function to calculate the result based on the operation
+func calculate(num1 int, operation string, num2 int) int {
+	switch strings.ToLower(operation) {
+	case "plus":
+		return num1 + num2
+	case "minus":
+		return num1 - num2
+	case "multiply":
+		return num1 * num2
+	case "divide":
+		if num2 != 0 {
+			return num1 / num2
+		}
+		return 0
+	default:
+		return 0
+	}
 }
